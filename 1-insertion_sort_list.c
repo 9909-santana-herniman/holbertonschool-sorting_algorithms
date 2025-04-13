b73837b40123c99f9661bf39834e209d7c704996
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-13 23:14:34 +0000

    Saving progress

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the insertion sort algorithm.
+ * @list: Pointer to the head of the doubly linked list.
+ */
+
+
